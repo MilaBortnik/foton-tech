d87dbd0a62f74636fea28486c353d98d63ffd672
MilaBortnik <mila_bortnik_1803@ukr.net>
2022-01-28 20:29:40 +0200

    add js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,51 @@
-const callButtonElement = document.querySelector('.button-main')
-const popupContainer = document.querySelector('.popup')
-callButtonElement.addEventListener('click', function() {popupContainer.classList.add('popup-body')})
+const CallButtonElement = document.querySelector('.button-main')
+const PopupContainer = document.querySelector('.popup');
+if(CallButtonElement) {
+    CallButtonElement.addEventListener('click', function() {PopupContainer.classList.add('popup-body')})
+}
 
-const middleButtonElement = document.querySelector('.popup-button')
-const popupMiddel=document.querySelector('.popup-middle')
-middleButtonElement.addEventListener('click', function() {popupMiddel.classList.add('popup-middel-body')})
+const MiddleButtonElement = document.querySelector('.popup-button')
+const PopupMiddel=document.querySelector('.popup-middle')
+MiddleButtonElement.addEventListener('click', function() {
+    PopupContainer.classList.remove('popup-body');
+    PopupMiddel.classList.add('popup-middle-body')
+})
 
-const finishButtonElement = document.querySelector('.popup-middle-button .popup-button')
+const FinishButtonElement = document.querySelector('.popup-middle-button .popup-button')
 const PopupFinish=document.querySelector('.popup-finish')
-finishButtonElement.addEventListener('click', function() {PopupFinish.classList.add('popup-finish-body')})
+FinishButtonElement.addEventListener('click', function() {
+    PopupMiddel.classList.remove('popup-middle-body');
+    PopupFinish.classList.add('popup-finish-body')
+})
 
 const FooterButtonElement = document.querySelector('.button-footer')
 const PopupFooter=document.querySelector('.popup-footer')
-FooterButtonElement.addEventListener('click', function(){PopupFooter.classList.add('popup-footer-body')})
+FooterButtonElement.addEventListener('click', function(){
+    PopupFooter.classList.add('popup-footer-body')
+})
 
+const FinishButtonOk=document.querySelector('.finish-btn .midlle-btn')
+FinishButtonOk.addEventListener('click', function() {
+    PopupFinish.classList.remove('popup-finish-body')
+})
+
+const CloseButton=document.querySelector('.closebtn')
+CloseButton.addEventListener('click', function(){
+    PopupContainer.classList.remove('popup-body')
+    PopupMiddel.classList.remove('popup-middle-body')
+})
+
+const CloseButtonMiddle=document.querySelector('.closebtn-middle .closebtn')
+CloseButtonMiddle.addEventListener('click', function() {
+    PopupMiddel.classList.remove('popup-middle-body')
+})
+
+const CloseButtonFinish=document.querySelector('.closebtn-finish .closebtn')
+CloseButtonFinish.addEventListener('click', function(){
+    PopupFinish.classList.remove('popup-finish-body')
+})
+
+const CloseButtonFooter=document.querySelector('.closebtn-footer .closebtn')
+CloseButtonFooter.addEventListener('click', function(){
+    PopupFooter.classList.remove('popup-footer-body')
+})
